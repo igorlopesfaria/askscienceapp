36775d82e891d73f0f1268917231f8317e1f6e07
u005522 <54jdellkq27zjfvl7phtxsgtfzhqn7roro3okwbrbt3qklwqydda>
2022-12-06 16:09:32 -0300

    - criando testes unitarios

--- data/test/user/data_source/user_local_data_source_test.dart
+++ data/test/user/data_source/user_local_data_source_test.dart
@@ -1,19 +1,19 @@
 import 'package:commons/report/reporter.dart';
 
-class MockReporter extends Mock implements IReporter {}
-
-void main() {
-  late IReporter mockReporter;
-  late ICredentialLocalDataSource mockDataSource;
-  late SharedPreferences prefs;
-
-  setUp(() async {
-    SharedPreferences.setMockInitialValues({
-      CredentialLocalDataSource.credentialAccessToken: "access_token",
-      CredentialLocalDataSource.credentialRefreshToken: "refresh_token"
-    });
-    prefs = await SharedPreferences.getInstance();
-    mockReporter = MockReporter();
-    mockDataSource = CredentialLocalDataSource(prefs, mockReporter);
-  });
-}
+// class MockReporter extends Mock implements IReporter {}
+//
+// void main() {
+//   late IReporter mockReporter;
+//   late ICredentialLocalDataSource mockDataSource;
+//   late SharedPreferences prefs;
+//
+//   setUp(() async {
+//     SharedPreferences.setMockInitialValues({
+//       CredentialLocalDataSource.credentialAccessToken: "access_token",
+//       CredentialLocalDataSource.credentialRefreshToken: "refresh_token"
+//     });
+//     prefs = await SharedPreferences.getInstance();
+//     mockReporter = MockReporter();
+//     mockDataSource = CredentialLocalDataSource(prefs, mockReporter);
+//   });
+// }
